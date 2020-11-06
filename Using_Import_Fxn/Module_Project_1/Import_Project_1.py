a4bcb1b56188632557ede5fa5234ea51c89dfd15
Shubham2405 <67518810+Shubham2405@users.noreply.github.com>
2020-11-06 23:53:34 +0530

    Add files via upload

---
+++ Using_Import_Fxn/Module_Project_1/Import_Project_1.py
@@ -0,0 +1,5 @@
+def Lbs_to_Kg(x):
+	return x/2.1
+
+def Kg_to_Lbs(x):
+	return x*2.1
---
+++ Using_Import_Fxn/Module_Project_1/Import_Project_2.py
@@ -0,0 +1,11 @@
+import Import_Project_1
+
+print(Import_Project_1.Kg_to_Lbs(5))
+
+from Import_Project_1 import Lbs_to_Kg
+
+print(Lbs_to_Kg(100))
+
+
+# We can directly call any specific fxn when we import it specificaly.
+# In contrast we have to call module name each time if we import entire module.
